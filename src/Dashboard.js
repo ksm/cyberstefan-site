a05debf0ef7f3c58da08631437ac758c3a658702
CyberStefan <hi@cyberstefan.com>
2020-06-13 18:01:46 -0400

    chore(dashboard): introduce widget to clean up and reuse code

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,75 +10,78 @@ import {
   Container,
 } from "reactstrap";
 
+const Widget = (props) => (
+  <div class="my-4">
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <CardTitle>{props.title}</CardTitle>
+            {props.children}
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+);
+
 export const Dashboard = () => {
   return (
     <Container>
-      <Row>
-        <Col>
-          <Card>
-            <CardBody>
-              <CardTitle>Upcoming Streams</CardTitle>
-              <Table striped>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>When</th>
-                    <th>What</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">6</th>
-                    <td>Saturday, May 30th, 2020</td>
-                    <td>CyberStefan.com Dashboard</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">7</th>
-                    <td>Saturday, June 6th, 2020</td>
-                    <td>
-                      TBD: let me know on Twitter! Possibly React Native app
-                      with TypeScript.
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <CardBody>
-              <CardTitle>Coding Projects</CardTitle>
-              <Table striped>
-                <thead>
-                  <tr>
-                    <th>Project</th>
-                    <th>Progress</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>CyberStefan.com Twitch Stream Dashboard</td>
-                    <td class="px-2">
-                      <Progress
-                        style={{ "min-width": 100 }}
-                        color="success"
-                        value="10"
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>React Native GitHub Client App</td>
-                    <td class="px-2">
-                      <Progress color="success" value="0" />
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+      <Widget title="Upcoming Streams">
+        <Table striped>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>When</th>
+              <th>What</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">6</th>
+              <td>Saturday, May 30th, 2020</td>
+              <td>CyberStefan.com Dashboard</td>
+            </tr>
+            <tr>
+              <th scope="row">7</th>
+              <td>Saturday, June 6th, 2020</td>
+              <td>
+                TBD: let me know on Twitter! Possibly React Native app with
+                TypeScript.
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Widget>
+      <Widget title="Coding Projects">
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Project</th>
+              <th>Progress</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>CyberStefan.com Twitch Stream Dashboard</td>
+              <td class="px-2">
+                <Progress
+                  style={{ "min-width": 100 }}
+                  color="success"
+                  value="10"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>React Native GitHub Client App</td>
+              <td class="px-2">
+                <Progress color="success" value="0" />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Widget>
     </Container>
   );
 };
