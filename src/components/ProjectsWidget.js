9176ab9653c4b4b3faae5f3109a720eacd7eac23
CyberStefan <hi@cyberstefan.com>
2020-06-27 18:56:20 -0400

    feat(dashboard): represent projects as json, calculate progress

--- src/components/ProjectsWidget.js
+++ src/components/ProjectsWidget.js
@@ -2,6 +2,20 @@ import React from "react";
 import { Table, Progress } from "reactstrap";
 import { Widget } from "./Widget";
 import { OctocatIcon } from "./OctocatIcon";
+import projects from "../data/projects.json";
+
+const calculateProjectProgress = (project) => {
+  const allMilestonesCount =
+    project.minimum_milestones > project.milestones.length
+      ? project.minimum_milestones
+      : project.milestones.length;
+
+  const completedMilestonesCount = project.milestones.filter(
+    (milestone) => milestone.status === "complete"
+  ).length;
+
+  return (completedMilestonesCount / allMilestonesCount) * 100;
+};
 
 export const ProjectsWidget = () => (
   <Widget title="Coding Projects">
@@ -13,28 +27,23 @@ export const ProjectsWidget = () => (
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>
-            CyberStefan.com Twitch Stream Dashboard{" "}
-            <a href="https://github.com/cyberstefantv/cyberstefan-site">
-              <OctocatIcon />
-            </a>
-          </td>
-          <td className="px-2">
-            <Progress style={{ minWidth: 100 }} color="success" value="20" />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            React Native GitHub Client App{" "}
-            <a href="https://github.com/cyberstefantv/cyberhub-app">
-              <OctocatIcon />
-            </a>
-          </td>
-          <td className="px-2">
-            <Progress color="success" value="10" />
-          </td>
-        </tr>
+        {projects.map((project) => (
+          <tr key={project.id}>
+            <td>
+              {project.name}{" "}
+              <a href={project.github}>
+                <OctocatIcon />
+              </a>
+            </td>
+            <td className="px-2">
+              <Progress
+                style={{ minWidth: 100 }}
+                color="success"
+                value={calculateProjectProgress(project)}
+              />
+            </td>
+          </tr>
+        ))}
       </tbody>
     </Table>
   </Widget>
