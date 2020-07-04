c7620c89fb42d3cbf3d4eebfadcf4e24a493446a
CyberStefan <hi@cyberstefan.com>
2020-07-04 18:46:04 -0400

    feat(header): add ability to collapse intro

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { Jumbotron } from "reactstrap";
+import React, { useState } from "react";
+import { Jumbotron, Collapse, Button } from "reactstrap";
 import signature from "./assets/signature.svg";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div className="mt-3">
-      <Jumbotron>
+      <Jumbotron className="py-4">
         <h2>
           <span role="img" aria-labelledby="jsx-a11y/accessible-emoji">
             👋
@@ -22,19 +25,26 @@ export const Header = () => {
           I stream every Saturday at 4 PM ET (that's 1 PM PT, or 22:00 CET if
           you're viewing from Europe).
         </p>
-        <p>
-          The idea behind this dashboard is to visualize and almost gamify the
-          stream's projects and milestones. That way you're more engaged and I'm
-          more motivated.
-        </p>
-        <p>
-          Find me discussing all things stream on{" "}
-          <a href="https://twitter.com/cyberstefantv">Twitter</a>.
-        </p>
-        <p>Yours sincerely,</p>
-        <p>
-          <img src={signature} alt="CyberStefan's signature" />
-        </p>
+        <Collapse isOpen={isOpen}>
+          <div>
+            <p>
+              The idea behind this dashboard is to visualize and almost gamify
+              the stream's projects and milestones. That way you're more engaged
+              and I'm more motivated.
+            </p>
+            <p>
+              Find me discussing all things stream on{" "}
+              <a href="https://twitter.com/cyberstefantv">Twitter</a>.
+            </p>
+            <p>Yours sincerely,</p>
+            <p>
+              <img src={signature} alt="CyberStefan's signature" />
+            </p>
+          </div>
+        </Collapse>
+        <Button className="p-0" color="link" onClick={toggle}>
+          {isOpen ? "Read less..." : "Read more..."}
+        </Button>
       </Jumbotron>
     </div>
   );
