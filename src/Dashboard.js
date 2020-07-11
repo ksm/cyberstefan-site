72718cd147aece70d8419c4c860a6e4e50596ad0
CyberStefan <hi@cyberstefan.com>
2020-07-11 19:27:41 -0400

    style: remove one container from hierarchy thus streamline margins

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Container } from "reactstrap";
 import { StreamsWidget, ProjectsWidget } from "./components";
 
 export const Dashboard = () => {
   return (
-    <Container>
+    <>
       <StreamsWidget />
       <ProjectsWidget />
-    </Container>
+    </>
   );
 };
